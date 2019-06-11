4240689b06ea9880b34a1ecb31e614a1e4a02a89
ihar <karpekin20001@gmail.com>
2019-06-11 16:34:35 +0300

    ADD: feautres

--- src/app/admin.control.event/admin.control.event.component.ts
+++ src/app/admin.control.event/admin.control.event.component.ts
@@ -1,15 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource, MatSnackBar } from '@angular/material';
+import { UserService } from '../shared/services/user.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import * as XLSX from 'xlsx';
+
+export interface EventGetModel {
+  EventId: number;
+  UserId: number;
+  Title: string;
+  Description: string;
+  Latitude: number;
+  Longitude: number;
+  Status: string;
+  CreationDate: string;
+  StartDate: string;
+  EndDate: string;
+}
 
 @Component({
-  selector: 'app-admin.control.event',
+  selector: 'app-admin-control-event',
   templateUrl: './admin.control.event.component.html',
   styleUrls: ['./admin.control.event.component.css']
 })
 export class AdminControlEventComponent implements OnInit {
+  public userData: EventGetModel[] = [
+    // tslint:disable-next-line:max-line-length
+    { EventId: 0, UserId: 1, Title: 'dsf', Description: 'asdfasdf', Latitude: 56.4578457, Longitude: 52345.555, Status: 'asdfasdf', CreationDate: '28/05/2019', StartDate: '28/05/2019', EndDate: '31/05/2019' },
+    // tslint:disable-next-line:max-line-length
+    { EventId: 1, UserId: 1, Title: 'dsf', Description: 'asdfasdf', Latitude: 56.4578457, Longitude: 52345.555, Status: 'asdfasdf', CreationDate: '28/05/2019', StartDate: '28/05/2019', EndDate: '31/05/2019' },
+    // tslint:disable-next-line:max-line-length
+    { EventId: 2, UserId: 1, Title: 'dsf', Description: 'asdfasdf', Latitude: 56.4578457, Longitude: 52345.555, Status: 'asdfasdf', CreationDate: '28/05/2019', StartDate: '28/05/2019', EndDate: '31/05/2019' },
+    // tslint:disable-next-line:max-line-length
+    { EventId: 3, UserId: 1, Title: 'dsf', Description: 'asdfasdf', Latitude: 56.4578457, Longitude: 52345.555, Status: 'asdfasdf', CreationDate: '28/05/2019', StartDate: '28/05/2019', EndDate: '31/05/2019' },
 
-  constructor() { }
+
+
+  ];
+  // public userData: UserGetModel[];
+  // tslint:disable-next-line:max-line-length
+  displayedColumns: string[] = ['EventId', 'UserId', 'Title', 'Description', 'Latitude', 'Longitude', 'Status', 'CreationDate', 'StartDate', 'EndDate', 'Delete'];
+
+
+  public dataSource: MatTableDataSource<EventGetModel>;
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('TABLE') table: ElementRef;
+
+
+  constructor(
+    private userService: UserService,
+    private http: HttpClient,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.userData);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  public expr(row) {
+    console.log(row);
+    debugger;
+    this.userData.forEach((element, idx) => {
+      if (element['UserId'] === row['UserId']) {
+        console.log(idx);
+        this.userData.splice(idx, 1);
+        this.dataSource._updateChangeSubscription();
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  public ExportTOExcel() {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'userTable.xlsx');
+
   }
 
 }
--- src/app/admin.control.pollution/admin.control.pollution.component.ts
+++ src/app/admin.control.pollution/admin.control.pollution.component.ts
@@ -1,15 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource, MatSnackBar } from '@angular/material';
+import { UserService } from '../shared/services/user.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import * as XLSX from 'xlsx';
+
+export interface PollutionGetModel {
+  PollutionId: number;
+  UserId: number;
+  Title: string;
+  Description: string;
+  Status: string;
+  CreationDate: string;
+}
 
 @Component({
-  selector: 'app-admin.control.pollution',
+  selector: 'app-admin-control-pollution',
   templateUrl: './admin.control.pollution.component.html',
   styleUrls: ['./admin.control.pollution.component.css']
 })
 export class AdminControlPollutionComponent implements OnInit {
+  public userData: PollutionGetModel[] = [
+    // tslint:disable-next-line:max-line-length
+    { PollutionId: 1, UserId: 1, Title: 'asdfasf', Description: 'asdfad', Status: 'Status', CreationDate: '29/08/2000' },
+    // tslint:disable-next-line:max-line-length
+    { PollutionId: 1, UserId: 1, Title: 'asdfasf', Description: 'asdfad', Status: 'Status', CreationDate: '29/08/2000' },
+    // tslint:disable-next-line:max-line-length
+    { PollutionId: 1, UserId: 1, Title: 'asdfasf', Description: 'asdfad', Status: 'Status', CreationDate: '29/08/2000' },
+    // tslint:disable-next-line:max-line-length
+    { PollutionId: 1, UserId: 1, Title: 'asdfasf', Description: 'asdfad', Status: 'Status', CreationDate: '29/08/2000' },
 
-  constructor() { }
+
+  ];
+  // public userData: UserGetModel[];
+  // tslint:disable-next-line:max-line-length
+  displayedColumns: string[] = ['PollutionId', 'UserId', 'Title', 'Description', 'Status', 'CreationDate', 'Delete'];
+
+
+  public dataSource: MatTableDataSource<PollutionGetModel>;
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('TABLE') table: ElementRef;
+
+
+  constructor(
+    private userService: UserService,
+    private http: HttpClient,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.userData);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+  public expr(row) {
+    console.log(row);
+    debugger;
+    this.userData.forEach((element, idx) => {
+      if (element['UserId'] === row['UserId']) {
+        console.log(idx);
+        this.userData.splice(idx, 1);
+        this.dataSource._updateChangeSubscription();
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  public ExportTOExcel() {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'userTable.xlsx');
+
+  }
 }
