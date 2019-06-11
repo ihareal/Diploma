ca96604b4c224b1940e1ddbd2a40ffab12bf98e0
ihar <karpekin20001@gmail.com>
2019-06-12 00:41:24 +0300

    ADD: cabinet features

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-statistic',
@@ -6,10 +7,165 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistic.component.css']
 })
 export class StatisticComponent implements OnInit {
-
+  public leadByIndustryChart: any;
   constructor() { }
 
   ngOnInit() {
+    this.initHousestypes();
+    this.generateHouseLegend();
+
+    this.initDistricts();
+    this.generateDistrictLegend();
+  }
+  private initHousestypes() {
+    const leadByIndustry = document.getElementById('house-type');
+    this.leadByIndustryChart = new Chart(leadByIndustry, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: [5, 10],
+          backgroundColor: ['red', 'yellow', 'blue', 'white'],
+        }],
+        labels: ['Flat', 'House']
+      },
+      options: {
+        animation: {
+          duration: 2000,
+        },
+        legendCallback: function (chart) {
+          const text = [];
+          text.push('<ul class="' + chart.id + '-legend">');
+          let total = 0;
+          for (let i = 0; i < chart.data.datasets[0].data.length; i++) {
+            total += +chart.data.datasets[0].data[i];
+          }
+          for (let i = 0; i < chart.data.datasets[0].data.length; i++) {
+            // tslint:disable-next-line:max-line-length
+            text.push('<li style="color:' + chart.data.datasets[0].backgroundColor[i] + '"><span style="color:' + chart.data.datasets[0].backgroundColor[i] + '">');
+            if (chart.data.datasets[0].data[i]) {
+              text.push(Math.floor(chart.data.datasets[0].data[i] / total * 100) + '% ');
+            }
+            text.push('</span>');
+            if (chart.data.labels[i]) {
+              text.push('<span style="color: #005874;font-weight: bold;font-size: 16px;">' + chart.data.labels[i]);
+            }
+            text.push('</li>');
+          }
+          text.push('</ul>');
+          return text.join('');
+        },
+        responsive: true,
+        legend: {
+          labels: {
+            usePointStyle: true,
+            fontFamily: 'Quicksand',
+            fontSize: 14,
+          },
+          display: false,
+          position: 'bottom',
+        },
+        tooltips: {
+          mode: 'nearest',
+          yAlign: 'bottom',
+          callbacks: {
+            title: function () {
+              return '';
+            },
+            label: function (tooltipItem, data) {
+              return data['datasets'][0]['data'][tooltipItem['index']];
+            }
+          },
+          backgroundColor: '#005874',
+          titleFontSize: 14,
+          titleFontFamily: 'Quicksand',
+          titleFontColor: '#FFFFFF',
+          bodyFontColor: '#FFFFFF',
+          bodyFontSize: 14,
+          displayColors: false
+        },
+      }
+    });
+  }
+
+  private generateHouseLegend() {
+    const legend = document.getElementById('house-type-legend');
+    legend.innerHTML = this.leadByIndustryChart.generateLegend();
+  }
+
+
+  private initDistricts() {
+    const leadByIndustry = document.getElementById('district');
+    this.leadByIndustryChart = new Chart(leadByIndustry, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: [1, 2, 3],
+          backgroundColor: ['green', 'yellow', 'white'],
+        }],
+        labels: ['Oktyabrskiy', 'Frunzenskiy', 'Centralniy']
+      },
+      options: {
+        animation: {
+          duration: 2000,
+        },
+        legendCallback: function (chart) {
+          const text = [];
+          text.push('<ul class="' + chart.id + '-legend">');
+          let total = 0;
+          for (let i = 0; i < chart.data.datasets[0].data.length; i++) {
+            total += +chart.data.datasets[0].data[i];
+          }
+          for (let i = 0; i < chart.data.datasets[0].data.length; i++) {
+            // tslint:disable-next-line:max-line-length
+            text.push('<li style="color:' + chart.data.datasets[0].backgroundColor[i] + '"><span style="color:' + chart.data.datasets[0].backgroundColor[i] + '">');
+            if (chart.data.datasets[0].data[i]) {
+              text.push(Math.floor(chart.data.datasets[0].data[i] / total * 100) + '% ');
+            }
+            text.push('</span>');
+            if (chart.data.labels[i]) {
+              text.push('<span style="color: #005874;font-weight: bold;font-size: 16px;">' + chart.data.labels[i]);
+            }
+            text.push('</li>');
+          }
+          text.push('</ul>');
+          return text.join('');
+        },
+        responsive: true,
+        legend: {
+          labels: {
+            usePointStyle: true,
+            fontFamily: 'Quicksand',
+            fontSize: 14,
+          },
+          display: false,
+          position: 'bottom',
+        },
+        tooltips: {
+          mode: 'nearest',
+          yAlign: 'bottom',
+          callbacks: {
+            title: function () {
+              return '';
+            },
+            label: function (tooltipItem, data) {
+              return data['datasets'][0]['data'][tooltipItem['index']];
+            }
+          },
+          backgroundColor: '#005874',
+          titleFontSize: 14,
+          titleFontFamily: 'Quicksand',
+          titleFontColor: '#FFFFFF',
+          bodyFontColor: '#FFFFFF',
+          bodyFontSize: 14,
+          displayColors: false
+        },
+      }
+    });
+  }
+
+  private generateDistrictLegend() {
+    const legend = document.getElementById('district-legend');
+    legend.innerHTML = this.leadByIndustryChart.generateLegend();
   }
 
 }
