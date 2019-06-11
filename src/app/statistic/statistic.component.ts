3cf875abaee4bb925f59f3baf908292bf9f2e994
ihar <karpekin20001@gmail.com>
2019-06-12 00:55:51 +0300

    ADD: Diagrams in admin cabinet

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -8,6 +8,8 @@ import { Chart } from 'chart.js';
 })
 export class StatisticComponent implements OnInit {
   public leadByIndustryChart: any;
+  public districtChart: any;
+  public readingTypeChart: any;
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +18,10 @@ export class StatisticComponent implements OnInit {
 
     this.initDistricts();
     this.generateDistrictLegend();
+
+    this.initReadingTime();
+    this.generateReadingLegend();
+
   }
   private initHousestypes() {
     const leadByIndustry = document.getElementById('house-type');
@@ -94,8 +100,8 @@ export class StatisticComponent implements OnInit {
 
 
   private initDistricts() {
-    const leadByIndustry = document.getElementById('district');
-    this.leadByIndustryChart = new Chart(leadByIndustry, {
+    const ctx = document.getElementById('district');
+    this.districtChart = new Chart(ctx, {
       type: 'pie',
       data: {
         datasets: [{
@@ -165,7 +171,78 @@ export class StatisticComponent implements OnInit {
 
   private generateDistrictLegend() {
     const legend = document.getElementById('district-legend');
-    legend.innerHTML = this.leadByIndustryChart.generateLegend();
+    legend.innerHTML = this.districtChart.generateLegend();
+  }
+
+
+  private initReadingTime() {
+    const ctx = document.getElementById('reading-time');
+    this.readingTypeChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: [500],
+          backgroundColor: ['green', 'yellow', 'white'],
+        }],
+        labels: []
+      },
+      options: {
+        animation: {
+          duration: 2000,
+        },
+        legendCallback: function (chart) {
+          const text = [];
+          text.push('<ul class="' + chart.id + '-legend">');
+          let total = 0;
+          for (let i = 0; i < chart.data.datasets[0].data.length; i++) {
+            total += +chart.data.datasets[0].data[i];
+          }
+          for (let i = 0; i < chart.data.datasets[0].data.length; i++) {
+            // tslint:disable-next-line:max-line-length
+            text.push('<li style="color:' + chart.data.datasets[0].backgroundColor[i] + '"><span style="color:' + chart.data.datasets[0].backgroundColor[i] + '">');
+            if (chart.data.datasets[0].data[i]) {
+              text.push(Math.floor(chart.data.datasets[0].data[i] / total * 100) + '% ');
+            }
+            text.push('</span>');
+            if (chart.data.labels[i]) {
+              text.push('<span style="color: #005874;font-weight: bold;font-size: 16px;">' + chart.data.labels[i]);
+            }
+            text.push('</li>');
+          }
+          text.push('</ul>');
+          return text.join('');
+        },
+        responsive: true,
+        legend: {
+          labels: {
+            usePointStyle: true,
+            fontFamily: 'Quicksand',
+            fontSize: 14,
+          },
+          display: false,
+          position: 'bottom',
+        },
+        tooltips: {
+          mode: 'nearest',
+          yAlign: 'bottom',
+          callbacks: {
+            title: function () {
+              return '';
+            },
+            label: function (tooltipItem, data) {
+              return data['datasets'][0]['data'][tooltipItem['index']] + ' minutes';
+            }
+          },
+          backgroundColor: '#005874',
+          titleFontSize: 14,
+          titleFontFamily: 'Quicksand',
+          titleFontColor: '#FFFFFF',
+          bodyFontColor: '#FFFFFF',
+          bodyFontSize: 14,
+          displayColors: false
+        },
+      }
+    });
   }
 
 }
