cb789d7e78d31f25e4931df77ca8a6ed0aae359e
ihar <karpekin20001@gmail.com>
2019-06-25 02:29:03 +0300

    Activities update

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -9,12 +9,14 @@ import { forkJoin } from 'rxjs';
 })
 
 export class ActivitiesComponent implements OnInit {
-    participate = [ ];
+    participate = [];
     public userId: any;
-    refuse = [ ];
+    refuse = [];
 
     public rootEventUrl = 'https://localhost:44338/api/EventDetails';
     public rootEventsByUser = 'https://localhost:44338/api/users/eventsByUser';
+    public rootPostEventsByUser = 'https://localhost:44338/api/users/events?';
+
 
     public allEvents = [];
     public eventsByUser = [];
@@ -34,8 +36,15 @@ export class ActivitiesComponent implements OnInit {
 
             if (event.container.id === 'cdk-drop-list-0') {
                 // post if sdk = 0 
+            ///    this.http.get(`https://localhost:44338/api/users/eventDelete?userId=${userId}&eventId=${eventId}`).subscribe(res => {
+            ///        console.log(res);
+            ///     });
             } else if (event.container.id === 'cdk-drop-list-1') {
                 // delete if sdk = 1
+             //   this.http.get(this.rootPostEventsByUser + `userId=${userId}&eventId=${eventId}`).subscribe(
+             //       res => { console.log(res); },
+             //       err => { console.log(err); }
+             //   );
             }
         }
     }
@@ -65,6 +74,6 @@ export class ActivitiesComponent implements OnInit {
     }
 
     ngOnInit() {
-       
+
     }
 }
